3ec2094c3f0fb4b595ce616fb66ea9279274c21d
Luca Fiore <lf44551@gmail.com>
2026-02-05 13:23:13 +0100

    Aggiungi la configurazione per servire asset statici dalla cartella 'public'

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Configurazione asset statici
+app.use(express.static('public'));
+
 const posts = [
     {
         titolo: "Ciambellone - La ricetta classica",
